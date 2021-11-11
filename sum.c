c9fc5bae777b83921dc7f4a87cd65ba8e2858078
AnayPanja <anaypanja30@gmail.com>
2021-11-11 19:57:25 +0530

    adding sum function

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(){
+    int a,b;
+    printf("modified \n");
+    printf("Enter first no. \n");
+    scanf("%d", &a);
+    printf("Enter second no. \n");
+        scanf("%d", &b);
+
+    printf("the sum is %d\n", a+b);
+    
+
+    return 0;
+}
